24bd69c36f322f69be19d4440bac6bea33c060d3
Subzwari1 <subzwarihasan@gmail.com>
2023-11-29 00:01:21 +0100

    Add DB URL

--- backend/data/database.py
+++ backend/data/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.orm import declarative_base
+import mysql.connector
 
-URL_DATABASE='mysql+pymysql://root:cloudcontact@localhost:3306/cloudcontactsapp'
-engine = create_engine(URL_DATABASE)
+URL_DATABASE = "mysql+pymysql://root:root@localhost:3306/cloud_db"
+
+engine = create_engine(URL_DATABASE, echo=True)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
+
