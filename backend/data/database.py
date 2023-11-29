6d1a9374b13b5b4c252f0538cf2e32c4d0a3a58c
Hasan Suzbwari <Subzwarihasan@gmail.com>
2023-11-29 02:17:25 +0100

    Update database.py

--- backend/data/database.py
+++ backend/data/database.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 URL_DATABASE='mysql+pymysql://root:helloworld@localhost:3306/cloudcontactsapp'
 
-engine = create_engine(URL_DATABASE, echo=True)
+engine = create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
 
