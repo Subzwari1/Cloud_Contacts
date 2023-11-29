06551ffa3e26bee522c9c8e1c3b4917b1c68e5cb
Hasan Suzbwari <Subzwarihasan@gmail.com>
2023-11-29 02:13:21 +0100

    Update database.py with the correct database name

--- backend/data/database.py
+++ backend/data/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 import mysql.connector
 
-URL_DATABASE = "mysql+pymysql://root:root@localhost:3306/cloud_db"
+URL_DATABASE='mysql+pymysql://root:helloworld@localhost:3306/cloudcontactsapp'
 
 engine = create_engine(URL_DATABASE, echo=True)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
