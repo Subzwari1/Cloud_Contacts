da49c49f1952a47accaef4cd5e388e0de066dd07
Hasan Suzbwari <Subzwarihasan@gmail.com>
2023-11-29 02:53:44 +0100

    adjusted code

--- backend/data/database.py
+++ backend/data/database.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 URL_DATABASE='mysql+pymysql://root:helloworld@localhost:3306/cloudcontactsapp'
-
 engine = create_engine(URL_DATABASE)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
-
