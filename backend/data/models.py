a96dbc327c4d7916f36df8dd62d45dfbc5ad16ec
Hasan Suzbwari <Subzwarihasan@gmail.com>
2023-11-29 02:56:42 +0100

    updated

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,15 +1,14 @@
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from data.database import Base
 
-class UserModel(Base):                   # changing user table's class name as UserModel
-   __tablename__='Users'
+class User(Base):
+   __tablename__='users'
 
-   id       =  Column(Integer,primary_key=True,index=True)
-   username =  Column(String(50),unique=True, index=True)
-   email    =  Column(String(50),unique=True, index=True)
-   password =  Column(String(50))
-   
+   id=Column(Integer,primary_key=True,index=True)
+   username=Column(String(50),unique=True)
+   password=Column(String(50))
+ 
 
 class Contact(Base):
    __tablename__='contacts'
@@ -21,6 +20,3 @@ class Contact(Base):
    email=Column(String(50))
    phone_number=Column(String(50))
    active=Column(Boolean, default=True)
-
-   
-  
