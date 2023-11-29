e97a3f6353d897a6b3b0c0d2cbdf1676b73f5014
Hasan Suzbwari <Subzwarihasan@gmail.com>
2023-11-29 02:18:10 +0100

    Update database.py

--- backend/data/database.py
+++ backend/data/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.orm import declarative_base
-import mysql.connector
 
 URL_DATABASE='mysql+pymysql://root:helloworld@localhost:3306/cloudcontactsapp'
 
